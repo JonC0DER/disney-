1ffe4b8a9a223e49e7620d0d72b269b63a5125e1
jonC0D3R <joncoder.kamite@gmail.com>
2024-04-08 17:26:27 +0200

    initialization of the project Disney+

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./components/Login";
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
