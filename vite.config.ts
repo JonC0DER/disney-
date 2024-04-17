80b53d0341a4bac5c8ad5648514a8bfe33a266de
jonC0D3R <joncoder.kamite@gmail.com>
2024-04-17 18:20:15 +0200

    adding CORS plugin but comment it

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, Plugin, ViteDevServer } from 'vite';
+import react from '@vitejs/plugin-react';
+// import {join} from 'path';
+// import {readFileSync} from 'fs';
+
+// const crossOriginIsolation: Plugin = {
+//   name: 'configure-server',
+
+//   configureServer(server: ViteDevServer) {
+//     server.middlewares.use((_req, res: import('http').ServerResponse, next: ()=> void) => {
+//       res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+//       res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+//       next();
+//     })
+//   },
+
+// };
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server:{
+    origin: 'http://localhost:5173/'
+  }
+});
